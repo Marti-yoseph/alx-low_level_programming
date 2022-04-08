fb8bc034148fc247aa14b9cbdb5a3a24a7dc70e1
Marti-yoseph <martayoseph998@gmail.com>
2022-04-08 14:34:20 -0700

    my first commit on postive or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 
 /**
- * main - Prints a random number and states whether it is postive, negative, or zero.
+ * main - Prints a random number and states whether it is postive, negative, or            zero.
  *
  * Return: Always 0.
  */
@@ -19,7 +19,6 @@ printf("%d is postive\n", n);
 else if (n < 0)
 printf("%d is negative\n", n);
 else
-
 printf("%d is zero\n", n);
 
 return (0);
