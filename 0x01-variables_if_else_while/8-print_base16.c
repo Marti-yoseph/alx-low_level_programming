d41cff6c73e7f4cb01360c742399afed16d7e910
Marti-yoseph <martayoseph998@gmail.com>
2022-04-08 16:43:51 -0700

    my first commit on print base16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - Entry point 
+ * main-Entry point 
  * Return:0
  */
 int main(void)
@@ -9,6 +9,8 @@ int main(void)
 char x;
 
 for (x = '0'; x <= '9'; ++x)
+putchar(x);
+for (x = 'a'; x <= 'f'; ++x)
 putchar(x);
 
 putchar('\n');
