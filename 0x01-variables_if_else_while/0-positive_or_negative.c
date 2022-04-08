425394532a7fddba999a2a628d3f02c5ba64a4a8
Marti-yoseph <martayoseph998@gmail.com>
2022-04-08 13:38:32 -0700

    my first commit on postive or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,13 @@
+#include <stdlio.h>
+#include <time.h>
 #include <stdio.h>
 
+
 /**
- * main - Entry point
+ * main - Prints a random number and states whether
+ *         it is postive, negative, or zero.
  *
- * Return: Always 0.
+ * Return: Always 0.  
  */
 int main(void)
 {
