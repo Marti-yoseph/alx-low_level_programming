8c786ad672b0883a2bf6fed9e4dddfc483cc0cc3
Marti-yoseph <martayoseph998@gmail.com>
2022-04-12 11:25:23 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - entry block
+ * @void: no argument
+ * Return: 0
+ **/
+int main(void)
+{
+char marta[] = "_putchar";
+
+int c;
+	
+for (c = 0; c < 8; c++)
+
+{
+_putchar(marta[c]);
+}
+_putchar('\n');
+return (0);
+}
