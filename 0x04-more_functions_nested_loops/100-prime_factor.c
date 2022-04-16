e4dd763ee3c7880dec6b2718a49fc92582c4e550
Marti-yoseph <martayoseph998@gmail.com>
2022-04-16 09:09:26 -0700

    my commit messsage

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,26 +1,46 @@
-#include <stdio.h>
+#include <stdio.h> 
 
-/**
- * main - prime factors of 612852475143
- *
- * Return: int
- */
+/** 
 
-int main(void)
-{
-unsigned long n, i, max;
 
-n = 612852475143;
-max = 1;
+  * main - entry block 
 
-for (i = 3; i <= n; i += 2)
-{
-while (n % i == 0)
-{
-max = n;
-n = n / i;
-}
-}
-printf("%1u\n", max);
-return (0);
-}
+
+  * @void: no argument 
+
+
+  * Return: 0 
+
+  */ 
+
+int main(void) 
+
+{ 
+
+
+long i, number = 612852475143; 
+
+
+for (i  = 2; i <= number; i++) 
+
+
+{ 
+
+
+if (number % i == 0) 
+
+{ 
+
+number = number / i; 
+
+i--; 
+
+} 
+
+} 
+
+printf("%lu\n", i); 
+
+return (0); 
+
+} 
