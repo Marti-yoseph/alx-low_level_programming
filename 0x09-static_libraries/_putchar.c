8834f47fe1357dd9aff59021774cf5c5f3f08ae7
Marti-yoseph <martayoseph998@gmail.com>
2022-05-01 10:32:12 -0700

    my commit message

---
+++ 0x09-static_libraries/_putchar.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ */
+
+int _putchar(char c);
+
+/**
+ * _1supper - check for uppercase character
+ * @c: charcter to be checked
+ * Return: 1 if true, 0 if false
+ */
+
+int _isupper(int c);
